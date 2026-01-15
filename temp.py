c648706cb11e89e3fa797363b65f1f6c9be082ae
SarvarAmirov <amirovsarvar90@gmail.com>
2026-01-15 23:37:11 +0500

    eeski mashqlar
    reg

---
+++ temp.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x = float(input('Biror juft son kiriting: '))
+if x==0:
+    print("Siz kiritgan son juft emas")
+elif x%2:
+    print("Siz kiritgan son juft emas")
+else:
+    print("Rahmat")
+
+
+
+
+yosh = int(input("Yoshingizni kiriting: "))
+if yosh <=4 or yosh>=60:
+    narx = 0
+elif yosh <18:
+    narx = 10000
+else:
+    narx = 20000
+
+print(f"Siz uchun muzyga kirish narxi {narx} so'm")
+
+
+
+
+
+# mahsulotlar = ['olma', 'anor', 'nok', 'anjir', 'non', 'suv', 'choy', 'kofe', 'sharbat', 'salat']
+# savat = []
+
+# print("5 ta mahsulot kiriting.")
+# for n in range(5):
+#     savat.append(input(f'{n+1}-mahsulotni kiriting: '))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for mahsulot in savat:
+#      if mahsulot in mahsulotlar:
+#          bor_mahsulotlar.append(mahsulot)
+#      else:
+#          mavjud_emas.append(mahsulot)
+
+# print(f"Do'konimizda {bor_mahsulotlar} bor")
+# print(f"Do'konimizda {mavjud_emas} yo'q")
+
+
+
+
+
+# foydalanuvchilar = ["log1", 'log2', 'log3', 'log4', 'log5']
+# yangi_login = input("yangi nom tanlang")
+
+# if yangi_login in foydalanuvchilar:
+#     print("Bu nom band")
+# else:
+#     print(f"Sizning nomingiz {yangi_login}")
+    
+    
+
+
+# x = int(input("Biror butun son kiriting: "))
+# if x%2 == 0:
+#     print(f"{x} soni 2 ga qoldiqsiz bo'linadi")
+
+# if x%3 == 0:
+#     print(f"{x} soni 3 ga qoldiqsiz bo'linadi")
+    
+# if x%4 == 0:
+#     print(f"{x} soni 4 ga qoldiqsiz bo'linadi")
+    
+# if x%5 == 0:
+#     print(f"{x} soni 5 ga qoldiqsiz bo'linadi")
+    
+# if x%6 == 0:
+#     print(f"{x} soni 6 ga qoldiqsiz bo'linadi")
+    
+# if x%7 == 0:
+#     print(f"{x} soni 7 ga qoldiqsiz bo'linadi")
+    
+# if x%8 == 0:
+#     print(f"{x} soni 8 ga qoldiqsiz bo'linadi")
+    
+# if x%9 == 0:
+#     print(f"{x} soni 9 ga qoldiqsiz bo'linadi")
+    
+# if x%10 == 0:
+#     print(f"{x} soni 10 ga qoldiqsiz bo'linadi")
+
+            
+# son = int(input("Istalgan burun son kiriting: "))
+# for n in range(2,101):
+#     if not (son%n):
+#         print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
